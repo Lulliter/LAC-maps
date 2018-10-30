76a9db694c2873ecf9b7b6b3c76fb5917fe710a2
Lulliter <lmm76@georgetown.edu>
2018-10-30 12:45:10 -0400

    3rd commit

--- build_site.R
+++ build_site.R
@@ -2,7 +2,8 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-setwd("/Users/luisamimmi/GoogleDrive/03_g_LAC/LAC-maps")
+setwd("~/GoogleDrive/03_g_LAC/LAC-maps")
 
 #render your sweet site. 
 rmarkdown::render_site()
+
